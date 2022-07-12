a3401bdf0c9dce1208fd39dbd85b065f128095df
Renato Tavares <renato.tavares.digital@gmail.com>
2022-07-12 14:30:25 -0300

    database2

--- src/controllers/cadastro.Controller.js
+++ src/controllers/cadastro.Controller.js
@@ -13,9 +13,9 @@ viewForm: (request, response) => {
 
 dadosSalvos: (req,res)=>{
     console.log(req.body) 
-    let { NomeDoAbrigo, Email, NomeProprietario, Capacidade, Numero ,Endereco , Bairro, Senha, ConfirmarSenha, sobre } = req.body;
-    let dadosAbrigos = JSON.stringify({ NomeDoAbrigo, Email, NomeProprietario, Capacidade, Numero ,Endereco , Bairro, Senha, ConfirmarSenha, sobre});
-    fs.writeFileSync(listaAbrigos, dadosAbrigos);
+    let { NomeDoAbrigo, Email, NomeProprietario, Capacidade, Numero ,Endereco , Bairro, Senha, ConfirmarSenha, sobre, sualogo } = req.body;
+    let dadosAbrigos = JSON.stringify([{ NomeDoAbrigo, Email, NomeProprietario, Capacidade, Numero ,Endereco , Bairro, Senha, ConfirmarSenha, sobre, sualogo}]);
+    fs.appendFileSync(listaAbrigos, dadosAbrigos);
     res.send('As informações do abrigo: ' + NomeDoAbrigo + ' foram enviadas com sucesso!')
 },
 }
