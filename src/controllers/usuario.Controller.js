323afd2c514923316005c1ed7c60fe97afd6c5a8
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2022-10-16 20:18:46 -0300

    add funcionalidade update

--- src/controllers/usuario.Controller.js
+++ src/controllers/usuario.Controller.js
@@ -1,12 +1,12 @@
 const {usuarioModel, abrigoModel, enderecoModel, contato_abrigoModel, socialModel} = require('../database');
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
 
 const usuarioController = {
-
-
-usuario: (request, response) => {
+  usuario: (request, response) => {
     response.render('usuario')
 },
-buscarUsuario: async (request, response) => {
+  buscarUsuario: async (request, response) => {
     const { id } = request.params;
 
     const usuario = await usuarioModel.findByPk(id, {include: {
@@ -28,39 +28,95 @@ buscarUsuario: async (request, response) => {
 
         }]
       }});
-      console.log(usuario);
+      /* console.log(usuario); */
     response.render('usuario', { usuario })
 },
 
-
-
-editarUsuario: async (request, response) => {
+/* editarUsuario: async (request, response) => {
     const { id } = request.params;
 
     const usuario = await usuarioModel.findByPk(id);
 
     response.render('usuario', { usuario })
-  },
+  }, */
 
   editar: async (request, response) => {
     const {  nome, sobrenome, email, suafoto, sualogo, celular, fixo, senha, cep, cidade, rua, complemento, bairro, numero, nomeAbrigo, emailAbrigo, sobre, tipo, contato, facebook, instagram  } = request.body;
+
     const { id } = request.params;
 
-    usuarioModel.update(
-        {  nome, sobrenome, email, suafoto, sualogo, celular, fixo, senha, cep, cidade, rua, complemento, bairro, numero, nomeAbrigo, emailAbrigo, sobre, tipo, contato, facebook, instagram  },
-        { where: { id } }
-    );
+      const editarUsuario =  usuarioModel.update({
+        nome,
+        sobrenome,
+        email,
+        celular,
+        fixo,
+        suafoto:request.files.suafoto[0].filename,
+        senha: bcrypt.hashSync(senha, salt),
+      }, { where: {id}});
 
+      const editarEndereco =  enderecoModel.update({
+        cep,
+        rua,
+        complemento,
+        bairro,
+        cidade,
+        numero
+       
+      }, { where: {id}});
+      
+      const [usuario, endereco] = await Promise.all([editarUsuario, editarEndereco]) 
+
+      const editarAbrigo = await abrigoModel.update({
+        usuarioId: usuario.id,
+        nomeAbrigo,
+        emailAbrigo,
+        sualogo:request.files.sualogo[0].filename,
+        enderecoId: endereco.id,
+        sobre
+      }, { where: {id}})
+
+      const editarContatos = await contato_abrigoModel.update({
+        tipo,
+        contato,
+        abrigoId: editarAbrigo.id
+      }, { where: {id}})
+
+      const editarSociais = await socialModel.update({
+        facebook,
+        instagram,
+        abrigoId: editarAbrigo.id
+      }, { where: {id}})
+      
     response.redirect(`/usuario/${id}`);
   },
 
-deleteUsuario: async (request, response) => {
+  deleteUsuarioTela:async (request, response) => {
     const { id } = request.params;
 
-    const movie = await usuarioModel.findByPk(id);
+    const usuario = await usuarioModel.findByPk(id, {include: {
+        model: abrigoModel,
+        as: 'abrigos',
+        include: [{
+            model: enderecoModel,
+            as: 'endereco'
 
-    response.render("usuarioDelete", { usuario });
-  },
+          },
+          {
+            model: socialModel,
+            as: 'sociais'
+  
+          },
+          {
+          model: contato_abrigoModel,
+          as: 'contatos'  
+
+        }]
+      }});
+      console.log(usuario.id);
+    response.render('usuarioDelete', { usuario })
+},
+
   delete: async (request, response) => {
     const { id } = request.params;
 
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,12 +1,38 @@
 const { Router }= require('express');
 const helpetController = require ('../controllers/usuario.Controller')
+const multer = require('multer');
+const path = require('path');
+
+const multerDiskStorage = multer.diskStorage({
+    destination: (req,file,callback)=>{
+       if(file.fieldname==='sualogo'){
+        callback(null,'public/profile')
+       }else{
+        callback(null,'public/foto')
+       }
+        
+    },
+    filename: (req,file,callback)=>{
+        callback(null,file.fieldname +'-'+ Date.now()+path.extname(file.originalname));
+    }
+});
+const upload = multer({storage: multerDiskStorage});
 
 const helpetRoutes = Router();
 
 helpetRoutes.get("/logout", helpetController.logout);
-helpetRoutes.get("/", helpetController.usuario);
-helpetRoutes.get("/:id", helpetController.buscarUsuario);
-helpetRoutes.put("/:id", helpetController.editar);
+
+helpetRoutes.get("/edit/:id", helpetController.buscarUsuario);
+helpetRoutes.put(
+    "/:id", 
+    upload.fields([
+        {name:'sualogo', maxCount:1},
+        {name:'suafoto', maxCount:1}
+    ]),
+    helpetController.editar);
+
+helpetRoutes.get("/delete/:id", helpetController.deleteUsuarioTela);
+helpetRoutes.delete("/:id", helpetController.delete);
 
 
 
