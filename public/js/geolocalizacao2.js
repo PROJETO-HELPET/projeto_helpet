e7808bc6958552bc763721ade702338435b32780
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2022-10-17 23:17:21 -0300

    organizando e revendo

--- public/js/geolocalizacao2.js
+++ public/js/geolocalizacao2.js
@@ -1,34 +1,34 @@
 function success(position) {
-    console.log(position.coords.latitude, position.coords.longitude)
-    let meuLocal = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    }
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 10,
-      center: meuLocal,
-    });
-    const marker = new google.maps.Marker({
-      position: meuLocal,
-      title: "Eu estou aqui!",
-      map: map,
-    });
-    const caoSemDono = new google.maps.Marker({
-      position: {lat: -23.631763531451902, lng:-46.60854484564916},
-      title: "Cão sem dono",
-      map: map,
-    });
-    const zoonoses= new google.maps.Marker({
-      position: {lat:-23.509428898465824, lng:-46.11413593863215},
-      title: "CCZ",
-      map: map,
-    });
-} 
+  console.log(position.coords.latitude, position.coords.longitude)
+  let meuLocal = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  }
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: meuLocal,
+  });
+  const marker = new google.maps.Marker({
+    position: meuLocal,
+    title: "Eu estou aqui!",
+    map: map,
+  });
+  const caoSemDono = new google.maps.Marker({
+    position: { lat: -23.631763531451902, lng: -46.60854484564916 },
+    title: "Cão sem dono",
+    map: map,
+  });
+  const zoonoses = new google.maps.Marker({
+    position: { lat: -23.509428898465824, lng: -46.11413593863215 },
+    title: "CCZ",
+    map: map,
+  });
+}
 
 function initMap() {
-  
-  let watchID = navigator.geolocation.watchPosition(success)
-  
+
+  let watchID = navigator.geolocation.watchPosition(success);
+
 }
 
 window.initMap = initMap;
